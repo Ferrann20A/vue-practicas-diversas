704a2dff771d7fe1e8a5009c3e59c9a629393480
Ferrann20A <alumno.398783@ies-azarquiel.es>
2023-11-02 09:07:36 +0100

    commit

---
+++ src/components/ConjeturaCollatz.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Conjetura Collatz</h1>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:"ConjeturaCollatz"
+    }
+</script>
+
+<style>
+    
+</style>
---
+++ src/components/MenuComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <ul id="menu">
+            <li>
+                <router-link to="/">Home</router-link>
+            </li>
+            <li>
+                <router-link to="/collatz">Conjetura de Collatz</router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default{
+        name:"MenuComponent"
+    }
+</script>
+
+<style>
+    ul#menu li{
+        display: inline;
+    }
+    li{
+        padding: 10px
+    }
+</style>
